99560a8cb60a78dc4afaa1bf5d25164972a4a306
Div Shekhar <div@txtlabs.com>
2017-03-28 20:44:49 -0700

    Update integration test

--- pyclient/integtest.py
+++ pyclient/integtest.py
@@ -4,18 +4,28 @@ from lockd import LockdClient
 
 lockd_client = LockdClient()
 
+# Initial state
+assert not lockd_client.IsLocked("foo")
+assert not lockd_client.IsLocked("bar")
+
 # Lock
-assert lockd_client.lock("foo")
+assert lockd_client.Lock("foo")
+assert lockd_client.IsLocked("foo")
 
 # Dup lock should fail
-assert not lockd_client.lock("foo")
+assert not lockd_client.Lock("foo")
+assert lockd_client.IsLocked("foo")
 
 # Lock another entry should work
-assert lockd_client.lock("bar")
+assert lockd_client.Lock("bar")
+assert lockd_client.IsLocked("bar")
 
 # Unlock entries
-assert lockd_client.unlock("foo")
-assert lockd_client.unlock("bar")
+assert lockd_client.Unlock("foo")
+assert lockd_client.Unlock("bar")
+assert not lockd_client.IsLocked("foo")
+assert not lockd_client.IsLocked("bar")
 
 # Dup unlock should fail
-assert not lockd_client.unlock("bar")
+assert not lockd_client.Unlock("bar")
+assert not lockd_client.IsLocked("bar")
