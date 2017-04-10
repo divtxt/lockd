8ac2611662f941b71dd1f28a818ab9ec2f4292ae
Div Shekhar <div@txtlabs.com>
2017-04-09 18:06:59 -0700

    Add is_locked calls to integration test

--- pyclient/integtest.py
+++ pyclient/integtest.py
@@ -4,18 +4,28 @@ from lockd import LockdClient
 
 lockd_client = LockdClient()
 
+# Initial state
+assert not lockd_client.is_locked("foo")
+assert not lockd_client.is_locked("bar")
+
 # Lock
 assert lockd_client.lock("foo")
+assert lockd_client.is_locked("foo")
 
 # Dup lock should fail
 assert not lockd_client.lock("foo")
+assert lockd_client.is_locked("foo")
 
 # Lock another entry should work
 assert lockd_client.lock("bar")
+assert lockd_client.is_locked("bar")
 
 # Unlock entries
 assert lockd_client.unlock("foo")
 assert lockd_client.unlock("bar")
+assert not lockd_client.is_locked("foo")
+assert not lockd_client.is_locked("bar")
 
 # Dup unlock should fail
 assert not lockd_client.unlock("bar")
+assert not lockd_client.is_locked("bar")
