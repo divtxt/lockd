2eda6b8ac6c7e1f06ea05ce5c9343d21f656a14b
Div Shekhar <div@txtlabs.com>
2017-04-16 17:32:08 -0700

    Add lock name validation method IsValidName()

--- statemachine/api.go
+++ statemachine/api.go
@@ -2,7 +2,9 @@ package statemachine
 
 // Interface to a Lock state machine implementation.
 //
-// Concurrence safety is not required by this interface.
+// Implementation notes:
+// - Concurrence safety is not required by this interface.
+// - Can panic for bad lock names - callers should already have validated names with IsValidName().
 //
 type LockStateMachine interface {
 	IsLocked(name string) bool
