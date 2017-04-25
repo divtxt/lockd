57102cfe1f718edd5d829984627a6807756bef1f
Div Shekhar <div@txtlabs.com>
2017-04-24 21:08:38 -0700

    Required flag -cluster

--- main.go
+++ main.go
@@ -3,17 +3,36 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+
+	"fmt"
 
 	"github.com/divtxt/lockd/ginx"
 	"github.com/divtxt/lockd/httpimpl"
 	"github.com/divtxt/lockd/lockimpl"
+	"github.com/divtxt/lockd/util"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Parse command line args
 	listenAddrPtr := flag.String("listen", ":2080", "listen address")
+	clusterPtr := flag.String(
+		"cluster",
+		"",
+		"cluster definition file - json file that describes the lockd cluster\n"+
+			"    \t    the json should be of the form: {\"server-id\": \"host:port\", ...}\n"+
+			"    \t    server ids should be positive integers, but as strings since json keys must be strings\n"+
+			"    \t    example: {\"1\": \"lockd1:2080\", \"2\": \"lockd2:2080\", \"3\": \"lockd3:2080\"}",
+	)
 	flag.Parse()
+
+	// Check required args
+	if *clusterPtr == "" {
+		fmt.Fprintln(os.Stderr, "Error: flag -cluster is required")
+		flag.Usage()
+		os.Exit(2)
+	}
 
 	// Reset standard log flags (undo Gin's settings)
 	log.SetFlags(log.LstdFlags)
@@ -21,9 +40,17 @@ func main() {
 	// Disable Gin debug logging
 	gin.SetMode(gin.ReleaseMode)
 
+	// Process args
+	log.Println("lockd initializing")
+	cd, err := util.LoadClusterDefinition(*clusterPtr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error loadig cluster definition: %v", err)
+		os.Exit(2)
+	}
+	log.Println("cluster:", cd)
+
 	// Instantiate a lock service
 	var l httpimpl.LockApi
-	var err error
 	l, err = lockimpl.NewLockApiImpl()
 	if err != nil {
 		panic(err)
---
+++ util/cluster_test.go
@@ -0,0 +1,28 @@
+package util_test
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+
+	"github.com/divtxt/lockd/util"
+	"github.com/divtxt/raft"
+)
+
+func TestClusterDefinition(t *testing.T) {
+	cd, err := util.LoadClusterDefinition("./cluster_test.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	asids := cd.GetAllServerIds()
+	sort.Slice(asids, func(i, j int) bool { return asids[i] < asids[j] })
+
+	if !reflect.DeepEqual(asids, []raft.ServerId{101, 102, 103}) {
+		t.Error(asids)
+	}
+
+	if cd.GetHostPort(102) != "10.0.0.22:2080" {
+		t.Error()
+	}
+}
