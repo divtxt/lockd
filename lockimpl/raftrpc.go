b60f11b1e1e5a070e270c3ddd8f18cdca0daf682
Div Shekhar <div@txtlabs.com>
2017-07-28 16:12:00 -0700

    Log rpc success/error changes instead of every error

--- lockimpl/raftrpc.go
+++ lockimpl/raftrpc.go
@@ -2,6 +2,7 @@ package lockimpl
 
 import (
 	"log"
+	"sync"
 
 	"fmt"
 
@@ -13,6 +14,10 @@ type JsonRaftRpcService struct {
 	cd           util.ClusterDefinition
 	thisServerId raft.ServerId
 	logger       *log.Logger
+
+	// mutable fields - use mutex for access!
+	mutex             sync.Mutex
+	_toServerErrCount map[raft.ServerId]uint // consecutive err count
 }
 
 func NewJsonRaftRpcService(
@@ -20,7 +25,17 @@ func NewJsonRaftRpcService(
 	thisServerId raft.ServerId,
 	logger *log.Logger,
 ) *JsonRaftRpcService {
-	return &JsonRaftRpcService{cd, thisServerId, logger}
+	tsec := make(map[raft.ServerId]uint)
+	for _, sid := range cd.GetAllServerIds() {
+		tsec[sid] = 0
+	}
+	return &JsonRaftRpcService{
+		cd,
+		thisServerId,
+		logger,
+		sync.Mutex{},
+		tsec,
+	}
 }
 
 func (jrrs *JsonRaftRpcService) RpcAppendEntries(
@@ -32,9 +47,10 @@ func (jrrs *JsonRaftRpcService) RpcAppendEntries(
 	var reply raft.RpcAppendEntriesReply
 	err := util.JsonPost(url, rpc, &reply)
 	if err != nil {
-		jrrs.logger.Printf("[lockd] rpc to %v -> error: %v\n", toServer, err)
+		jrrs.logRpcError(toServer, err)
 		return nil
 	}
+	jrrs.logRpcSuccess(toServer)
 	return &reply
 }
 
@@ -47,8 +63,31 @@ func (jrrs *JsonRaftRpcService) RpcRequestVote(
 	var reply raft.RpcRequestVoteReply
 	err := util.JsonPost(url, rpc, &reply)
 	if err != nil {
-		jrrs.logger.Printf("[lockd] rpc to %v -> error: %v\n", toServer, err)
+		jrrs.logRpcError(toServer, err)
 		return nil
 	}
+	jrrs.logRpcSuccess(toServer)
 	return &reply
+}
+
+func (jrrs *JsonRaftRpcService) logRpcSuccess(toServer raft.ServerId) {
+	jrrs.mutex.Lock()
+	defer jrrs.mutex.Unlock()
+	if jrrs._toServerErrCount[toServer] > 0 {
+		jrrs.logger.Printf(
+			"[lockd] rpc to %v -> success after %v consecutive errors\n",
+			toServer,
+			jrrs._toServerErrCount[toServer],
+		)
+		jrrs._toServerErrCount[toServer] = 0
+	}
+}
+
+func (jrrs *JsonRaftRpcService) logRpcError(toServer raft.ServerId, err error) {
+	jrrs.mutex.Lock()
+	defer jrrs.mutex.Unlock()
+	if jrrs._toServerErrCount[toServer] == 0 {
+		jrrs.logger.Printf("[lockd] rpc to %v -> starting to error: %v\n", toServer, err)
+	}
+	jrrs._toServerErrCount[toServer]++
 }
